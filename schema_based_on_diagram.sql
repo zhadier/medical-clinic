5614a94f46bfbb4e693772569c1bd974a95826f5
Bondok6 <kirolousbondok6@gmail.com>
2022-03-29 22:07:08 +0200

    Create invoices and invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,3 +18,22 @@ CREATE TABLE treatments (
     type VARCHAR(100) NOT NULL ,
 	name VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE invoices(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP NOT NULL,
+    medical_history_id INT NOT NULL REFERENCES medical_histories(id),
+);
+
+CREATE TABLE invoice_items(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL NOT NULL,
+    invoice_id INT NOT NULL,
+    treatment_id INT NOT NULL REFERENCES treatments(id),
+);
+
+
