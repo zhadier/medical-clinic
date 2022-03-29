bcdebd295433a4bc48b776f461a0eef2e1fa9d2d
Zeeshan Haider <zhadier39@gmail.com>
2022-03-30 00:35:24 +0500

    add medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,4 +6,11 @@ CREATE TABLE patients (
     date_of_birth DATE NOT NULL
 );
 
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP NOT NULL,
+    patient_id INT NOT NULL REFERENCES patients(id),
+	status VARCHAR(100) NOT NULL
+);
+
 
